f43ebc83b92ae5fe9fa048b0c061fb92f8c98a15
JesusJimenez4 <robotcat242@gmail.com>
2023-11-06 19:16:20 -0600

    Api meals

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Card  from "./components/Card";
+import LetterSelector from "./components/LetterSelector";
+import axios from "axios";
+
+function App() {
+
+  const [meals, setMeals] = useState([])
+
+  const getMeals = (letter = 'a') => {
+    axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+    .then((res) => {
+      if (res.data.meals) {
+        setMeals(res.data.meals)
+      } else {
+        setMeals([])
+      }
+    })
+  }
+
+  useEffect(() =>{
+    getMeals()
+  }, [])
+
+  return (
+    <div >
+      <LetterSelector onSelect={(letter) => getMeals(letter)}/>
+      <main className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+
+        {
+          meals.map((meal) => (
+            
+            <Card 
+            key={meal.idMeal}
+            strMeal={meal.strMeal}
+            strInstructions={meal.strInstructions}
+            strMealThumb={meal.strMealThumb}
+        />
+
+          ))
+        }
+      </main>
+    </div>
+  );
+}
+
+export default App;
